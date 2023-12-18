0c484e2359ca9ef0097f1ac34d1a29d9e1d88279
7Hassan <egoker1234@gmail.com>
2023-12-17 18:09:32 -0800

    updating

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ *  main - Entry point
+ * Description: A C program that prints with put function
+ * Return Always  0 (success)
+*/
+
 int main(void)
 {
   printf("Size of a char: %lu byte(s)\n", sizeof(char));
