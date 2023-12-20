cb15d6dbaf4334299db639fedf5df651f47d0077
Hassan Hossam <96396366+7Hassan@users.noreply.github.com>
2023-12-20 17:56:58 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main(char c)
+int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
-	
+
 	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
-	
+
 	return (0);
 }
